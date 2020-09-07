aacc067ab19ffa5dc90eb6d378302b216e792b93
GertrudaSK <penkta@gmail.com>
2020-09-07 15:11:19 +0300

    Car microservice tested on Postman
    Execute HTTP requests like in task example using Postman

--- car/car_microservice/__init__.py
+++ car/car_microservice/__init__.py
@@ -3,6 +3,7 @@ from flask_json import FlaskJSON
 from flask_sqlalchemy import SQLAlchemy
 import os
 
+
 app = Flask(__name__)
 FlaskJSON(app)
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -10,3 +11,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,
                                                                     'cars.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+import routes
+
